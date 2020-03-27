43b0408385874dc8ab23b50437d2022b3517980b
Loic <loicvieu@outlook.fr>
2020-03-27 10:31:30 +0100

    Renommage

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Lecture du fichier "data/textesimple.txt"
+        Lecture ALire = new Lecture();
+
+        // Création de l'arbre
+        Constructeur tree = new Constructeur(ALire);
+
+        //Création du dictionnaire de comparaison
+        Encodage encoda = new Encodage(tree);
+
+         }
+}
